8516a756023a936220f7e88c8e91c741f6ad43d0
신희성 <vvwv3092@gmail.com>
2023-09-15 19:53:13 +0900

    init

---
+++ src/main/java/com/jwtpractice/demo/TlsgmltjdApplication.java
@@ -0,0 +1,13 @@
+package com.jwtpractice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlsgmltjdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlsgmltjdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwtpractice/demo/TlsgmltjdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtpractice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlsgmltjdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
