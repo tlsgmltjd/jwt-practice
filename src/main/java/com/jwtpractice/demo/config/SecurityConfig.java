b0fb02063f43dc1256179aecb9eabdcca8208787
신희성 <vvwv3092@gmail.com>
2023-09-21 20:01:00 +0900

    Add JWT, SecurityConfig

--- src/main/java/com/jwtpractice/demo/config/SecurityConfig.java
+++ src/main/java/com/jwtpractice/demo/config/SecurityConfig.java
@@ -1,31 +1,79 @@
 package com.jwtpractice.demo.config;
 
+import com.jwtpractice.demo.jwt.JwtAccessDeniedHandler;
+import com.jwtpractice.demo.jwt.JwtAuthenticationEntryPoint;
+import com.jwtpractice.demo.jwt.JwtSecurityConfig;
+import com.jwtpractice.demo.jwt.TokenProvider;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.CorsFilter;
 
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 @Configuration
-@EnableWebSecurity // 기본적인 Web 보인을 활성화 하겠다.
 public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    @Bean
-    public WebSecurityCustomizer configure(){
-        return (web) -> web.ignoring()
-                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
-                .requestMatchers(new AntPathRequestMatcher("/favicon.ico"));
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            CorsFilter corsFilter,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.corsFilter = corsFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .authorizeRequests()
-                .requestMatchers(new AntPathRequestMatcher("/api/hello")).permitAll()
-                .anyRequest().authenticated();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                )
+
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/hello")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/authenticate")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/signup")).permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .headers(headers ->
+                        headers.frameOptions(options ->
+                                options.sameOrigin()
+                        )
+                )
+
+                .apply(new JwtSecurityConfig(tokenProvider));
         return http.build();
     }
 }
